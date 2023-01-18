c0ca1b1a77692d24e25784aee500a4869c009103
tycho garen <garen@tychoish.com>
2023-01-18 07:00:35 -0500

    wrap: docs update

--- wrap.go
+++ wrap.go
@@ -3,7 +3,7 @@ package fun
 // Is a generic version of `errors.Is` that takes advantage of the
 // Unwrap function, and is useful for checking if an object of an
 // interface type is or wraps an implementation of the type
-// parameter. Callers
+// parameter.
 func Is[T any](in any) bool {
 	for {
 		if _, ok := in.(T); ok {
@@ -17,7 +17,7 @@ func Is[T any](in any) bool {
 
 // Unwrap is a generic equivalent of the `errors.Unwrap()` function
 // for any type that implements an `Unwrap() T` method. useful in
-// combination
+// combination with Is.
 func Unwrap[T any](in T) T {
 	u, ok := any(in).(interface{ Unwrap() T })
 	if !ok {
