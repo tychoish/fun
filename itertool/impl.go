87ba807588b4728639980edcf40444303a6fee7e
tycho garen <garen@tychoish.com>
2023-01-20 17:38:21 -0500

    itertool: docstring

--- itertool/impl.go
+++ itertool/impl.go
@@ -1,3 +1,6 @@
+// Package itertool provides a set of functional helpers for
+// managinging and using fun.Iterator implementations, including a
+// parallel Map/Reduce, Merge, and other convenient tools.
 package itertool
 
 import (
