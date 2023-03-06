2158999cd9d4c7662d27d15319b184b8ecda6b86
tycho garen <garen@tychoish.com>
2023-03-06 12:06:01 -0500

    fun: zero wrappers

--- wrap.go
+++ wrap.go
@@ -21,7 +21,44 @@ func Is[T any](in any) bool {
 func Unwrap[T any](in T) T {
 	u, ok := any(in).(interface{ Unwrap() T })
 	if !ok {
-		return *new(T)
+		return ZeroOf[T]()
 	}
 	return u.Unwrap()
+}
+
+// Zero returns the zero-value for the type T of the input argument.
+func Zero[T any](in T) T { return ZeroOf[T]() }
+
+// ZeroOf returns the zero-value for the type T specified as an
+// argument.
+func ZeroOf[T any]() T { return *new(T) }
+
+// IsZero returns true if the input value compares "true" to the zero
+// value for the type of the argument. If the type implements an
+// IsZero() method (e.g. time.Time), then IsZero returns that value,
+// otherwise, IsZero constructs a zero valued object of type T and
+// compares the input value to the zero value.
+func IsZero[T comparable](in T) bool {
+	switch val := any(in).(type) {
+	case interface{ IsZero() bool }:
+		return val.IsZero()
+	default:
+		return val == Zero(in)
+	}
+}
+
+// ZeroWhenNil takes a value of any type, and if that value is nil,
+// returns the zero value of the specified type. Otherwise,
+// ZeroWhenNil coerces the value into T and returns it. If the input
+// value does not match the output type of the function, ZeroWhenNil
+// panics with an ErrInvariantViolation.
+func ZeroWhenNil[T any](val any) T {
+	if val == nil {
+		return ZeroOf[T]()
+	}
+	out, ok := val.(T)
+
+	Invariant(ok, "unexpected type mismatch")
+
+	return out
 }
--- wrap_test.go
+++ wrap_test.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"testing"
+	"time"
+
+	"github.com/tychoish/fun/assert"
 )
 
 type wrapTestType struct {
@@ -33,5 +36,33 @@ func TestWrap(t *testing.T) {
 		if unwrapped != err {
 			t.Fatal("unexpected unrwapping")
 		}
+	})
+}
+func TestZeroHelpers(t *testing.T) {
+	t.Run("Zero", func(t *testing.T) {
+		assert.Zero(t, Zero(100))
+		assert.Zero(t, Zero(true))
+		assert.Zero(t, Zero("hello world"))
+	})
+	t.Run("Predicate", func(t *testing.T) {
+		assert.True(t, !IsZero(100))
+		assert.True(t, !IsZero(true))
+		assert.True(t, !IsZero("hello world"))
+		assert.True(t, IsZero(0))
+		assert.True(t, IsZero(false))
+		assert.True(t, IsZero(""))
+		assert.True(t, IsZero(time.Time{}))
+	})
+	t.Run("OrNil", func(t *testing.T) {
+		assert.Zero(t, ZeroWhenNil[int](any(0)))
+		assert.True(t, ZeroWhenNil[bool](any(true)))
+		assert.True(t, !ZeroWhenNil[bool](any(false)))
+		assert.Equal(t, "hello world", ZeroWhenNil[string](any("hello world")))
+		assert.NotZero(t, ZeroWhenNil[time.Time](time.Now()))
+
+		var foo = "foo"
+		var tt testing.TB
+		assert.NotZero(t, ZeroWhenNil[*string](&foo))
+		assert.Zero(t, ZeroWhenNil[*testing.T](tt))
 	})
 }
