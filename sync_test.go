dfd586c50630a0e936d15e3116ef4794423e25e9
tycho garen <garen@tychoish.com>
2023-03-07 18:31:26 -0500

    sync: additional wait group test

--- sync_test.go
+++ sync_test.go
@@ -1,7 +1,10 @@
 package fun
 
 import (
+	"context"
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/tychoish/fun/assert"
 )
@@ -20,4 +23,47 @@ func TestAtomic(t *testing.T) {
 		at.Set(42)
 		assert.Equal(t, at.Get(), 42)
 	})
+}
+
+func TestWaitGroup(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	wg := &WaitGroup{}
+	wg.Add(100)
+	start := time.Now()
+	firstCase := make(chan struct{})
+	go func() { wg.Wait(ctx) }()
+
+	go func() {
+		defer close(firstCase)
+
+		wg.Wait(ctx)
+	}()
+
+	runtime.Gosched()
+
+	secondCase := make(chan struct{})
+	go func() {
+		defer close(secondCase)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+		defer cancel()
+		wg.Wait(ctx)
+	}()
+	runtime.Gosched()
+
+	<-secondCase
+
+	timeoutDur := time.Since(start)
+	if timeoutDur > 11*time.Millisecond {
+		t.Error("timeout waiter deadlocked", timeoutDur)
+	}
+	time.Sleep(10 * time.Millisecond)
+	cancel()
+
+	<-firstCase
+
+	blockingDur := time.Since(start)
+	if blockingDur-timeoutDur > 11*time.Millisecond {
+		t.Error("blocking waiter deadlocked", blockingDur, timeoutDur)
+	}
 }
