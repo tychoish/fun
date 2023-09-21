0304ae1dfef16da7b7deb6cc4cab22c58b07bc12
tycho garen <garen@tychoish.com>
2023-09-20 23:22:30 -0400

    iter: add Tee implementation

--- iterator.go
+++ iterator.go
@@ -608,9 +608,34 @@ func (i *Iterator[T]) ParallelBuffer(n int) *Iterator[T] {
 	return buf.Producer().PreHook(pipe).IteratorWithHook(func(si *Iterator[T]) { si.AddError(i.Close()) })
 }
 
-func (i *Iterator[T]) Tee(n int,
+func (i *Iterator[T]) Tee(
+	n int,
 	opts ...OptionProvider[*WorkerGroupConf],
 ) []*Iterator[T] {
+	if n == 0 {
+		return nil
+	}
 
-	return nil
+	out := make([]*Iterator[T], n)
+	pipes := make([]ChanSend[T], n)
+	for idx := range out {
+		p := Blocking(make(chan T))
+		pipes[idx] = p.Send()
+		if idx == 0 {
+			out[idx] = p.Receive().Producer().PreHook(func(ctx context.Context) {
+				i.ProcessParallel(func(ctx context.Context, item T) error {
+					for _, pipe := range pipes {
+						if err := pipe.Write(ctx, item); err != nil {
+							return err
+						}
+					}
+					return nil
+				}, opts...)
+			}).Iterator()
+		} else {
+			out[idx] = p.Iterator()
+		}
+	}
+
+	return out
 }
