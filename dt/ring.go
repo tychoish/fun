d958bd54c8086f2a234560c010502708f8398028
tycho garen <garen@tychoish.com>
2025-03-24 13:16:44 -0400

    chore: complete testing

--- dt/ring.go
+++ dt/ring.go
@@ -1,11 +1,6 @@
 package dt
 
 import (
-	"context"
-	"fmt"
-	"io"
-	"sync"
-
 	"github.com/tychoish/fun"
 	"github.com/tychoish/fun/ft"
 	"github.com/tychoish/fun/intish"
@@ -14,45 +9,59 @@ import (
 const maxRingSize int = 4294967296
 const defaultRingSize int = 1024
 
+// Ring is a simple generic ring-buffer implemented on top of a slice/array
+// with a few conveniences: there are forward and backward iterators;
+// you can pop items from the "end" (oldest) in the buffer. The
+// Total() method maintains a count of the total number of items added
+// to the buffer.
+//
+// Operations on the Ring are NOT safe for concurrent use from
+// multiple go routines.
 type Ring[T any] struct {
 	size  int
 	count int
 	pos   int
 	total uint64
 	buf   struct {
-		once sync.Once
 		ring []T
-		nils []*T // this could be a bitmap but that sounds fussy.
+		nils []*T
 	}
 }
 
+func (r *Ring[T]) init() { ft.WhenCall(r.buf.ring == nil, r.innerInit) }
 func (r *Ring[T]) innerInit() {
-	fun.Invariant.IsTrue(r.size < maxRingSize, "invalid size", r.size)
+	r.size = ft.Default(r.size, defaultRingSize)
 
-	r.size = ft.Default(max(2, r.size), 1024)
+	fun.Invariant.IsTrue(r.size < maxRingSize, "invalid size", r.size, "max:", maxRingSize)
+	fun.Invariant.IsTrue(r.size >= 2, "invalid size", r.size, "(must be > 1)")
+
 	r.buf.ring = make([]T, r.size)
 	r.buf.nils = make([]*T, r.size)
 }
 
-func (r *Ring[T]) init()          { r.buf.once.Do(r.innerInit) }
-func (r *Ring[T]) set(i int, v T) { r.init(); r.buf.ring[i] = v; r.buf.nils[i] = &r.buf.ring[i] }
-func (r *Ring[T]) get(idx int) T  { r.init(); return r.buf.ring[idx] }
+func (r *Ring[T]) Setup(size int) { r.size = size }
+func (r *Ring[T]) Cap() int       { return r.size }
+func (r *Ring[T]) Len() int       { return r.count }
+func (r *Ring[T]) Total() uint64  { return r.total }
 
-func (r *Ring[T]) Cap() int               { return r.size }
-func (r *Ring[T]) Setup(size int)         { r.size = size }
-func (r *Ring[T]) Len() int               { return r.count }
-func (r *Ring[T]) Total() uint64          { return r.total }
-func (r *Ring[T]) Tail() T                { return r.get(r.curentPos()) }
-func (r *Ring[T]) Head() T                { return r.get(r.oldestPos()) }
-func (r *Ring[T]) FIFO() *fun.Iterator[T] { return r.iterate(r.oldestPos(), r.after) }
-func (r *Ring[T]) LIFO() *fun.Iterator[T] { return r.iterate(r.curentPos(), r.before) }
+func (r *Ring[T]) Head() T                { r.init(); return r.buf.ring[r.oldest()] }
+func (r *Ring[T]) Tail() T                { r.init(); return r.buf.ring[r.before(r.pos)] }
+func (r *Ring[T]) FIFO() *fun.Iterator[T] { r.init(); return r.iterate(r.oldest(), r.after) }
+func (r *Ring[T]) LIFO() *fun.Iterator[T] { r.init(); return r.iterate(r.before(r.pos), r.before) }
+
+func (*Ring[T]) zero() (out T)            { return out }
+func (r *Ring[T]) offset(idx, by int) int { return intish.AbsMax(r.size, r.size*idx) + (idx + by) }
+func (r *Ring[T]) oldest() int            { return ft.IfValue(r.count < r.size, 0, r.pos) }
+func (r *Ring[T]) after(idx int) int      { return r.offset(idx, 1) % r.size }
+func (r *Ring[T]) before(idx int) int     { return r.offset(idx, -1) % r.size }
 
 func (r *Ring[T]) Push(val T) {
 	r.init()
 
-	r.set(r.pos, val)
+	r.buf.ring[r.pos] = val
+	r.buf.nils[r.pos] = &r.buf.ring[r.pos]
 
-	r.pos = r.nextPos()
+	r.pos = r.after(r.pos)
 
 	r.total++
 	if r.count < r.size {
@@ -60,46 +69,43 @@ func (r *Ring[T]) Push(val T) {
 	}
 }
 
-func (r *Ring[T]) nextPos() int       { return r.after(r.pos) }
-func (r *Ring[T]) curentPos() int     { return r.pos }
-func (r *Ring[T]) oldestPos() int     { return ft.IfDo(r.count < r.size, ft.Wrapper(0), r.nextPos) }
-func (r *Ring[T]) after(idx int) int  { return (idx + 1) % r.size }
-func (r *Ring[T]) before(idx int) int { return ((r.size * intish.Abs(idx)) + (idx - 1)) % r.size }
+func (r *Ring[T]) Pop() *T {
+	r.init()
+	for idx := r.oldest(); r.count > 0; idx = r.after(idx) {
+		if r.buf.nils[idx] == nil {
+			continue
+		}
+
+		item := r.buf.ring[idx]
+		r.buf.ring[idx] = r.zero()
+		r.buf.nils[idx] = nil
+		r.count--
+		return &item
+	}
+
+	return nil
+}
 
 func (r *Ring[T]) iterate(from int, advance func(int) int) *fun.Iterator[T] {
-	// var prev int
-	var next int
+	var count int
 	var current int
 
-	var zero T
-	next = from
-	var count int
+	next := from
 
-	return fun.NewProducer(func(ctx context.Context) (T, error) {
-		if count == len(r.buf.ring) {
-			return zero, io.EOF
-		}
-		if err := ctx.Err(); err != nil {
-			return zero, err
-		}
-
-		// advance the index.
+	return fun.CheckProducer(func() (T, bool) {
 		for {
-			// prev = current
+			if count >= len(r.buf.ring) || (next == from && count > 0) {
+				return r.zero(), false
+			}
+
 			current = next
 			next = advance(current)
 
+			count++
+
 			if r.buf.nils[current] != nil {
-				break
+				return r.buf.ring[current], true
 			}
-			// current is nil and also if...
-			if next == from && r.buf.nils[next] == nil {
-				return zero, io.EOF
-			}
-			// otherwise skip to the next (this supports us popping)
 		}
-		fmt.Println(count, r.buf.ring[current], r.buf.ring[next], r.buf.ring)
-		count++
-		return r.buf.ring[current], nil
 	}).Iterator()
 }
--- dt/ring_test.go
+++ dt/ring_test.go
@@ -1,9 +1,13 @@
 package dt
 
 import (
+	"context"
 	"testing"
 
 	"github.com/tychoish/fun/assert"
+	"github.com/tychoish/fun/assert/check"
+	"github.com/tychoish/fun/ft"
+	"github.com/tychoish/fun/testt"
 )
 
 func TestRing(t *testing.T) {
@@ -22,22 +26,149 @@ func TestRing(t *testing.T) {
 		t.Run("FIFO", func(t *testing.T) {
 			items, err := ring.FIFO().Slice(t.Context())
 			assert.NotError(t, err)
-			t.Log(items)
 			assert.Equal(t, len(items), ring.Len())
 			// ordering:
-			assert.Equal(t, items[0], 42)
-			assert.Equal(t, items[1], 84)
+			check.Equal(t, items[0], 42)
+			check.Equal(t, items[1], 84)
 		})
 		t.Run("LIFO", func(t *testing.T) {
-			items, err := ring.FIFO().Slice(t.Context())
+			items, err := ring.LIFO().Slice(t.Context())
 			assert.NotError(t, err)
-			t.Log(items)
 			assert.Equal(t, len(items), ring.Len())
 			// ordering:
 			assert.Equal(t, items[0], 84)
 			assert.Equal(t, items[1], 42)
 		})
+		t.Run("Canceled", func(t *testing.T) {
+			ctx, cancel := context.WithCancel(t.Context())
+			cancel()
 
+			items, err := ring.LIFO().Slice(ctx)
+			assert.Error(t, err)
+			assert.ErrorIs(t, err, context.Canceled)
+			assert.Equal(t, len(items), 0)
+		})
+	})
+	t.Run("Zero", func(t *testing.T) {
+		ring := &Ring[int]{}
+
+		assert.Zero(t, ring.Head())
+		assert.Zero(t, ring.Tail())
+		assert.Equal(t, ring.Cap(), 1024)
+		assert.Equal(t, 0, ring.Len())
 	})
 
+	t.Run("Setup", func(t *testing.T) {
+		ring := &Ring[int]{}
+		ring.Setup(2048)
+
+		assert.Zero(t, ring.Head())
+		assert.Zero(t, ring.Tail())
+		assert.Equal(t, ring.Cap(), 2048)
+		assert.Equal(t, 0, ring.Len())
+	})
+	t.Run("Overload", func(t *testing.T) {
+		t.Run("Limit", func(t *testing.T) {
+			ring := &Ring[int]{}
+			ring.Setup(8)
+
+			for range 16 {
+				ring.Push(2)
+			}
+
+			assert.Equal(t, ring.Len(), 8)
+			assert.Equal(t, ring.Cap(), 8)
+			assert.Equal(t, ring.Total(), 16)
+			assert.Equal(t, sum(ft.Must(ring.FIFO().Slice(t.Context()))), 2*8)
+		})
+
+		t.Run("Order", func(t *testing.T) {
+			t.Run("FIFO", func(t *testing.T) {
+				ring := &Ring[int]{}
+				ring.Setup(5)
+
+				for range 5 {
+					for v := range 5 {
+						ring.Push(v)
+					}
+				}
+
+				assert.Equal(t, ring.Len(), 5)
+				assert.Equal(t, ring.Cap(), 5)
+				assert.Equal(t, ring.Total(), 25)
+
+				fifo := ft.Must(ring.FIFO().Slice(t.Context()))
+				testt.Log(t, fifo)
+				expected := []int{0, 1, 2, 3, 4}
+				for idx := range fifo {
+					check.Equal(t, fifo[idx], expected[idx])
+				}
+
+			})
+			t.Run("LIFO", func(t *testing.T) {
+				ring := &Ring[int]{}
+				ring.Setup(5)
+
+				for range 5 {
+					for v := range 5 {
+						ring.Push(v)
+					}
+				}
+
+				assert.Equal(t, ring.Len(), 5)
+				assert.Equal(t, ring.Cap(), 5)
+				assert.Equal(t, ring.Total(), 25)
+
+				lifo := ft.Must(ring.LIFO().Slice(t.Context()))
+				expected := []int{4, 3, 2, 1, 0}
+				testt.Log(t, lifo)
+				for idx := range lifo {
+					check.Equal(t, lifo[idx], expected[idx])
+				}
+			})
+		})
+	})
+	t.Run("Pop", func(t *testing.T) {
+		ring := &Ring[int]{}
+
+		for idx := range 5 {
+			ring.Push(idx)
+		}
+		assert.Equal(t, ring.Len(), 5)
+
+		testt.Log(t, ft.Must(ring.FIFO().Slice(t.Context())))
+
+		assert.Equal(t, ring.Total(), 5)
+		for idx := range 5 {
+			v := ring.Pop()
+			assert.NotNil(t, v)
+			check.Equal(t, *v, idx)
+		}
+		assert.Equal(t, ring.Len(), 0)
+		assert.Equal(t, ring.Total(), 5)
+	})
+	t.Run("OverPop", func(t *testing.T) {
+		ring := &Ring[int]{}
+		assert.Nil(t, ring.Pop())
+	})
+	t.Run("HeadAndTail", func(t *testing.T) {
+		ring := &Ring[int]{}
+
+		for idx := range 5 {
+			ring.Push(idx)
+			check.Equal(t, ring.Tail(), idx)
+		}
+
+		check.Equal(t, ring.Head(), 0)
+		check.Equal(t, ring.Tail(), 4)
+	})
+
+}
+
+func sum(in []int) int {
+	out := 0
+	for _, v := range in {
+		out += v
+	}
+	return out
 }
