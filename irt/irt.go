5ac0b0f0f883a47e5e150070cb2d4dd2e5f4cbdf
tycho <garen@tychoish.com>
2025-12-20 17:43:16 -0500

    feat: irt when operations

--- irt/irt.go
+++ irt/irt.go
@@ -19,14 +19,14 @@ func Collect2[K comparable, V any](seq iter.Seq2[K, V], args ...int) map[K]V {
 	return mp
 }
 
+func CollectFirstN[T any](seq iter.Seq[T], n int) []T { return nil }
+
 func Slice[T any](sl []T) iter.Seq[T]                     { return slices.Values(sl) }
 func Args[T any](items ...T) iter.Seq[T]                  { return Slice(items) }
 func Map[K comparable, V any](mp map[K]V) iter.Seq2[K, V] { return maps.All(mp) }
 
 func WithIndex[T any](seq iter.Seq[T]) iter.Seq2[int, T] { return Flip(Zip(seq, wrap[T](counter()))) }
 func JoinErrors(seq iter.Seq[error]) error               { return errors.Join(slices.Collect(seq)...) }
-
-func Contains[T comparable](seq iter.Seq[T], cmp T) bool { return false }
 
 func Flip[A, B any](seq iter.Seq2[A, B]) iter.Seq2[B, A] { return nil }
 func First[A, B any](seq iter.Seq2[A, B]) iter.Seq[A]    { return nil }
@@ -52,8 +52,9 @@ func ApplyAll12[A, B any](seq iter.Seq2[A, B], op func(A, B) error) error  { ret
 func Channel[T any](ctx context.Context, ch <-chan T) iter.Seq[T] { return nil }
 func Pipe[T any](ctx context.Context, seq iter.Seq[T]) <-chan T   { return nil }
 
-func Chain[T any](seq iter.Seq[iter.Seq[T]]) iter.Seq[T] { return nil }
-func ChainSlices[T any](seq iter.Seq[[]T]) iter.Seq[T]   { return nil }
+func Chunk[T any](seq iter.Seq[T], num int) iter.Seq[iter.Seq[T]] { return nil }
+func Chain[T any](seq iter.Seq[iter.Seq[T]]) iter.Seq[T]          { return nil }
+func ChainSlices[T any](seq iter.Seq[[]T]) iter.Seq[T]            { return Chain(Convert(seq, slices.Values)) }
 
 func Keep[T any](seq iter.Seq[T], prd func(T) bool) iter.Seq[T]                  { return nil }
 func Keep2[A, B any](seq iter.Seq2[A, B], prd func(A, B) bool) iter.Seq2[A, B]   { return nil }
@@ -63,7 +64,41 @@ func Remove2[A, B any](seq iter.Seq2[A, B], prd func(A, B) bool) iter.Seq2[A, B]
 func GroupBy[K comparable, V any](input iter.Seq[V], groupBy func(V) K) iter.Seq2[K, []V] { return nil }
 func Group[K comparable, V any](seq iter.Seq2[K, V]) iter.Seq2[K, []V]                    { return nil }
 func Unique[T comparable](seq iter.Seq[T]) iter.Seq[T]                                    { return nil }
-func UniqueB[K comparable, V any](seq iter.Seq[V], kfn func(V) K) iter.Seq[V]             { return nil }
+func UniqueBy[K comparable, V any](seq iter.Seq[V], kfn func(V) K) iter.Seq[V]            { return nil }
+
+func Reduce[T any](seq iter.Seq[T], rfn func(T, T) T) (out T) {
+	for v := range seq {
+		out = rfn(out, v)
+	}
+	return out
+}
+
+func Contains[T comparable](seq iter.Seq[T], cmp T) (ok bool) {
+	ApplyWhile(seq, func(in T) bool { ok = (cmp == in); return !ok })
+	return
+}
+
+func Equal[T comparable](rhs iter.Seq[T], lhs iter.Seq[T]) bool {
+	rhNext, rhStop := iter.Pull(rhs)
+	defer rhStop()
+	lhNext, lhStop := iter.Pull(lhs)
+	defer lhStop()
+	for {
+		rhv, okr := rhNext()
+		lhv, okl := lhNext()
+		if okr != okl {
+			return false
+		}
+		if !okr && !okl {
+			return true
+		}
+		if rhv != lhv {
+			return false
+		}
+	}
+}
+
+// Tuple wrapper type
 
 type Elem[A, B any] struct {
 	First  A
@@ -75,11 +110,12 @@ func Elems[A, B any](seq iter.Seq2[A, B]) iter.Seq[Elem[A, B]] { return Merge(se
 
 // simple implementation methods
 
-func counter() func() int              { var count int; return func() int { count++; return count } }
-func seen[T comparable]() func(T) bool { s := set[T]{}; return s.add }
+func counter() func() int               { var count int; return func() int { count++; return count } }
+func seen[T comparable]() func(T) bool  { s := set[T]{}; return s.add }
+func equal[T comparable](lh, rh T) bool { return lh == rh }
 
-func first[A any, B any](a A, b B) A           { return a }
-func second[A any, B any](a A, b B) B          { return b }
+func first[A any, B any](a A, _ B) A           { return a }
+func second[A any, B any](_ A, b B) B          { return b }
 func wrap[A any, B any](op func() B) func(A) B { return func(A) B { return op() } }
 func noop[T any](in T) T                       { return in }
 
@@ -112,6 +148,13 @@ func derefzero[T any](in *T) T        { return ifdoelsedo(isNil(in), zero[T], de
 
 func idx[E any, S ~[]E](idx int, sl S) E            { return sl[idx] }
 func idxlazy[E any, S ~[]E](idx int, sl S) func() E { return func() E { return sl[idx] } }
+
+func funcall[T any](op func(T), arg T)         { op(arg) }
+func funcallv[T any](op func([]T), args ...T)  { op(args) }
+func funcallr[A, B any](op func(A) B, arg A) B { return op(arg) }
+
+func orDefault[T comparable](val T, defaultValue T) T { return ifelse(isZero(val), defaultValue, val) }
+func orDefaultNew[T comparable](val T, op func() T) T { return ifdoelse(isZero(val), op, val) }
 
 func idxorz[E any, S ~[]E](idx int, sl S) E {
 	// TODO: performance compare
@@ -151,8 +194,26 @@ func ifdoelsedo[T any](cond bool, then func() T, elsewise func() T) T {
 	return elsewise()
 }
 
-func orDefault[T comparable](val T, defaultValue T) T { return ifelse(isZero(val), defaultValue, val) }
-func orDefaultNew[T comparable](val T, op func() T) T { return ifdoelse(isZero(val), op, val) }
+func ifcallelsecall[T any](cond bool, then func(), elsewise func()) {
+	if cond {
+		then()
+	} else {
+		elsewise()
+	}
+}
+
+func whendo[T any](cond bool, then func() T) (out T) {
+	if cond {
+		out = then()
+	}
+	return
+}
+
+func whencall[T any](cond bool, then func(T), arg T) {
+	if cond {
+		then(arg)
+	}
+}
 
 type none struct{}
 
@@ -161,3 +222,4 @@ type set[T comparable] map[T]none
 func (s set[T]) check(v T) bool         { _, ok := s[v]; return ok }
 func (s set[T]) set(v T)                { s[v] = none{} }
 func (s set[T]) add(v T) (existed bool) { existed = s.check(v); s.set(v); return }
+func (s set[T]) iter() iter.Seq[T]      { return maps.Keys(s) }
