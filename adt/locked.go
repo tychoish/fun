a08fdee834f2960857ece4f543080ffa39f6447a
tycho <garen@tychoish.com>
2026-01-12 18:57:09 -0500

    chore: moving code around to keep methods closer to the type

--- adt/locked.go
+++ adt/locked.go
@@ -15,6 +15,38 @@ type Synchronized[T any] struct {
 	obj T
 }
 
+// NewSynchronized constructs a new synchronized object that wraps the
+// input type.
+func NewSynchronized[T any](in T) *Synchronized[T] { return &Synchronized[T]{obj: in} }
+
+// With runs the input function within the lock, to mutate the object.
+func (s *Synchronized[T]) With(in func(obj T)) { defer With(Lock(&s.mtx)); in(s.obj) }
+
+// Set overrides the current value of the protected object.
+func (s *Synchronized[T]) Set(in T) { s.Store(in) }
+
+// Store replaces the value of the protected with the new provided value.
+func (s *Synchronized[T]) Store(in T) { s.Using(func() { s.obj = in }) }
+
+// String implements fmt.Stringer using this type.
+func (s *Synchronized[T]) String() string { return fmt.Sprint(s.Get()) }
+
+// Get returns the underlying protected object.
+func (s *Synchronized[T]) Get() T { return s.Load() }
+
+// Load returns the underlyting protected object.
+func (s *Synchronized[T]) Load() T { defer With(Lock(&s.mtx)); return s.obj }
+
+// Using runs the provided operation while holding the lock, but
+// without providing access to the locked value.
+func (s *Synchronized[T]) Using(op func()) { defer With(Lock(&s.mtx)); op() }
+
+// Swap sets the locked value to the new value and returns the old.
+func (s *Synchronized[T]) Swap(newVal T) (old T) {
+	s.Using(func() { old = s.obj; s.obj = newVal })
+	return
+}
+
 // Lock takes the lock, locks it, and then returns it.
 //
 // This, in combination With() makes it possible to have a single
@@ -51,35 +83,3 @@ func WithW(m *sync.RWMutex) { m.Unlock() }
 // LockW takes a RWMutex, locks as a write, and returns it to be
 // unlocked by WithW.
 func LockW(m *sync.RWMutex) *sync.RWMutex { m.Lock(); return m }
-
-// NewSynchronized constructs a new synchronized object that wraps the
-// input type.
-func NewSynchronized[T any](in T) *Synchronized[T] { return &Synchronized[T]{obj: in} }
-
-// With runs the input function within the lock, to mutate the object.
-func (s *Synchronized[T]) With(in func(obj T)) { defer With(Lock(&s.mtx)); in(s.obj) }
-
-// Set overrides the current value of the protected object.
-func (s *Synchronized[T]) Set(in T) { s.Store(in) }
-
-// Store replaces the value of the protected with the new provided value.
-func (s *Synchronized[T]) Store(in T) { s.Using(func() { s.obj = in }) }
-
-// String implements fmt.Stringer using this type.
-func (s *Synchronized[T]) String() string { return fmt.Sprint(s.Get()) }
-
-// Get returns the underlying protected object.
-func (s *Synchronized[T]) Get() T { return s.Load() }
-
-// Load returns the underlyting protected object.
-func (s *Synchronized[T]) Load() T { defer With(Lock(&s.mtx)); return s.obj }
-
-// Using runs the provided operation while holding the lock, but
-// without providing access to the locked value.
-func (s *Synchronized[T]) Using(op func()) { defer With(Lock(&s.mtx)); op() }
-
-// Swap sets the locked value to the new value and returns the old.
-func (s *Synchronized[T]) Swap(newVal T) (old T) {
-	s.Using(func() { old = s.obj; s.obj = newVal })
-	return
-}
